a35a532975d8b916069ced6e028252bcf05d45e2
Kevin Denker <oficialdenker@gmail.com>
2025-05-12 12:22:32 -0300

    Update schedules.module.ts

--- src/Modules/Schedules/Modules/schedules.module.ts
+++ src/Modules/Schedules/Modules/schedules.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
-import { ScheduleController } from '../Controllers/schedules.controller';
 import { ScheduleService } from '../Services/schedules.service';
+import { ScheduleController } from '../Controllers/schedules.controller';
+import { JwtModule } from '@nestjs/jwt';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '7d' },
+    }),
+  ],
   controllers: [ScheduleController],
   providers: [ScheduleService],
 })
