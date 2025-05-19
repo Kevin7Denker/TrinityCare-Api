849637775155e503cc5a1dcd3d0fde64c6298332
Kevin Denker <oficialdenker@gmail.com>
2025-05-19 17:04:52 -0300

    fix

--- src/Modules/Doctors/Modules/doctor.module.ts
+++ src/Modules/Doctors/Modules/doctor.module.ts
@@ -3,10 +3,12 @@ import { DoctorService } from '../Services/doctor.service';
 import { DoctorController } from '../Controllers/doctor.controller';
 import { JwtModule } from '@nestjs/jwt';
 import * as dotenv from 'dotenv';
+import { EmailModule } from 'src/Common/Emails/Modules/email.module';
 dotenv.config();
 
 @Module({
   imports: [
+    EmailModule,
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: '7d' },
